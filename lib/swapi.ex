061a109d74bd69a4c070106cc8bc4d8940f7d29d
Tim Whitacre <tim@timw.co>
2016-03-06 14:29:13 -0500

    adding other endpoints

--- lib/swapi.ex
+++ lib/swapi.ex
@@ -1,25 +1,99 @@
 defmodule Swapi do
-  @moduledoc """
-  Simple wrapper for the Star Wars API
-  """
+  @moduledoc "Simple wrapper for the Star Wars API"
   alias HTTPoison.Response
 
-  @doc """
-  Gets an individual person from the Star Wars Universe
-
-  Example:
-    iex> Swapi.people(1)
-    {:ok,  {...}}
-  """
-  def people(id) do
-    "http://swapi.co/api/people/#{id}/"
+  @doc "Gets the root url with a list of available resources"
+  def root do
+    nil
+    |> build_url
     |> get_data
   end
+
+  @doc "Gets an individual or list of people from the Star Wars Universe"
+  def people(id \\ nil) do
+    if id do
+      {"people", id}
+    else
+      "people"
+    end
+    |> build_url
+    |> get_data
+  end
+
+  @doc "Gets an individual or list of films from the Star Wars Universe"
+  def films(id) do
+    if id do
+      {"films", id}
+    else
+      "films"
+    end
+    |> build_url
+    |> get_data
+  end
+
+  @doc "Gets an individual or list of vehicles from the Star Wars Universe"
+  def vehicles(id) do
+    if id do
+      {"vehicles", id}
+    else
+      "vehicles"
+    end
+    |> build_url
+    |> get_data
+  end
+
+  @doc "Gets an individual or list of starships from the Star Wars Universe"
+  def starships(id) do
+    if id do
+      {"starships", id}
+    else
+      "starships"
+    end
+    |> build_url
+    |> get_data
+  end
+
+  @doc "Gets an individual or list of planets from the Star Wars Universe"
+  def planets(id) do
+    if id do
+      {"planets", id}
+    else
+      "planets"
+    end
+    |> build_url
+    |> get_data
+  end
+
+  @doc "Gets an individual or list of species from the Star Wars Universe"
+  def species(id) do
+    if id do
+      {"species", id}
+    else
+      "species"
+    end
+    |> build_url
+    |> get_data
+  end
+
 
   defp get_data(url) do
     url
     |> HTTPoison.get
     |> get_body
+  end
+
+  defp build_url({path, id}) do
+    "http://swapi.co/api/#{path}/#{id}/"
+    |> IO.inspect
+  end
+
+  defp build_url(path) do
+    if path do
+      "http://swapi.co/api/#{path}/"
+    else
+      "http://swapi.co/api/"
+    end
+    |> IO.inspect
   end
 
   defp get_body({:ok, %Response{status_code: 404}}), do: {:error, "Resource not found"}
--- test/swapi_test.exs
+++ test/swapi_test.exs
@@ -1,8 +1,63 @@
 defmodule SwapiTest do
   use ExUnit.Case, async: true
-  
-  test "person with id returns the correct person" do
-    assert {:ok, _} = Swapi.people(1)
+
+  # # Root Endpoint Test
+  # test "root returns a succesffull response" do
+  #   assert {:ok, _} = Swapi.root
+  # end
+  #
+  # # People Endpoint Tests
+  # test "person without id returns successful response" do
+  #   assert {:ok, _} = Swapi.people
+  # end
+  #
+  # test "person with id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.people(1)
+  # end
+  #
+  # # Film Endpoing Tests
+  # test "film without id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.films
+  # end
+  #
+  # test "film with id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.films(1)
+  # end
+  #
+  # # Vehicle Endpoint Tests
+  # test "vehicle without id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.vehicles
+  # end
+  #
+  # test "vehicle with id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.vehicles(4)
+  # end
+
+  # Starship Endpoint Tests
+  test "starship without id returns a succesfull response" do
+    assert {:ok, _} = Swapi.starships
   end
+
+  test "starship with id returns a succesfull response" do
+    assert {:ok, _} = Swapi.starships(9)
+  end
+
+  # # Species Endpoint Tests
+  # test "species without id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.species
+  # end
+  #
+  # test "species with id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.species(1)
+  # end
+  #
+  # # Plant Endpoint Tests
+  # test "planet without id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.planets
+  # end
+  #
+  # test "planet with id returns a succesfull response" do
+  #   assert {:ok, _} = Swapi.planets(1)
+  # end
 
 end
