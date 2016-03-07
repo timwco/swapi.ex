85520b4cc5c0dbaf5a3ed5d7f64c04c8b1c0797e
Tim Whitacre <tim@timw.co>
2016-03-06 19:41:17 -0500

    updated the docs

--- lib/swapi.ex
+++ lib/swapi.ex
@@ -2,56 +2,114 @@ defmodule Swapi do
   # @moduledoc "Simple wrapper for the Star Wars API"
   alias HTTPoison.Response
 
-  @doc "Gets the root url with a list of available resources"
+  @doc """
+  Gets the root url with a list of available resources
+  Example:
+    iex> Swapi.root
+    {:ok,  {...}}
+  """
   def root do
     nil
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of people from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of people from the Star Wars Universe
+  Example:
+    iex> Swapi.people
+    {:ok,  {[...]}}
+
+    iex> Swapi.people(1)
+    {:ok, {...}}
+  """
   def people(id \\ nil) do
     if id do {"people", id} else "people" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of films from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of films from the Star Wars Universe
+  Example:
+    iex> Swapi.films
+    {:ok,  {[...]}}
+
+    iex> Swapi.films(1)
+    {:ok, {...}}
+  """
   def films(id \\ nil) do
     if id do {"films", id} else "films" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of vehicles from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of vehicles from the Star Wars Universe
+  Example:
+    iex> Swapi.vehicles
+    {:ok,  {[...]}}
+
+    iex> Swapi.vehicles(1)
+    {:ok, {...}}
+  """
   def vehicles(id \\ nil) do
     if id do {"vehicles", id} else "vehicles" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of starships from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of starships from the Star Wars Universe
+  Example:
+    iex> Swapi.starships
+    {:ok,  {[...]}}
+
+    iex> Swapi.starships(1)
+    {:ok, {...}}
+  """
   def starships(id \\ nil) do
     if id do {"starships", id} else "starships" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of planets from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of planets from the Star Wars Universe
+  Example:
+    iex> Swapi.planets
+    {:ok,  {[...]}}
+
+    iex> Swapi.planets(1)
+    {:ok, {...}}
+  """
   def planets(id \\ nil) do
     if id do {"planets", id} else "planets" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets an individual or list of species from the Star Wars Universe"
+  @doc """
+  Gets an individual or list of species from the Star Wars Universe
+  Example:
+    iex> Swapi.species
+    {:ok,  {[...]}}
+
+    iex> Swapi.species(1)
+    {:ok, {...}}
+  """
   def species(id \\ nil) do
     if id do {"species", id} else "species" end
     |> build_url
     |> get_data
   end
 
-  @doc "Gets the schema for any specific resource"
+  @doc """
+  Gets the schema for any specific resource
+  Example:
+    iex> Swapi.schema("people")
+    {:ok,  {...}}
+  """
   def schema(resource) do
     "http://swapi.co/api/#{resource}/schema"
     |> get_data
